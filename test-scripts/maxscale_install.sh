278d5150a004123c15fce71b23b018d0c79537a4
Timofey Turenko <timofey.turenko@skysql.com>
2014-06-25 23:34:45 +0200

    fix maxscale_install.sh

--- test-scripts/maxscale_install.sh
+++ test-scripts/maxscale_install.sh
@@ -7,8 +7,11 @@
 set -x
 
 image=$1
-IP=$1
+IP=$2
+target=$3
 
 cat /home/ec2-user/test-scripts/maxscale.repo.template | sed "s/###target###/$target/" | sed "s/###image###/$image/" >  /home/ec2-user/test-scripts/maxscale.repo
-scp -i /home/ec2-user/KEYS/$image  -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /home/ec2-user/test-scripts/maxscale.repo root@192.168.122.102:/etc/yum.repos.d/
-ssh -i /home/ec2-user/KEYS/$image  -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$IP "yum clean all; yum -y install maxscale"
+scp -i /home/ec2-user/KEYS/$image -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /home/ec2-user/test-scripts/maxscale.repo root@$IP:/etc/yum.repos.d/
+scp -i /home/ec2-user/KEYS/$image -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /home/ec2-user/yum_files/$image/* root@$IP:/etc/yum.repos.d/
+
+ssh -i /home/ec2-user/KEYS/$image -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$IP "yum clean all; yum -y install maxscale"
