8fb4800ae035e0565c37ab254ded91b99dc230dd
Timofey Turenko <timofey.turenko@skysql.com>
2014-10-22 20:22:34 +0200

    fix run_system_test

--- test-scripts/run_system_test.sh
+++ test-scripts/run_system_test.sh
@@ -4,19 +4,7 @@
 
 export Test_name=$1
 
-template_line=`cat /usr/local/skysql/maxscale/system-test/templates | grep $Test_name`
-a=( $template_line )
-template=${a[1]}
-
-
 . /usr/local/skysql/maxscale/system-test/set_env_f.sh $replicationIP $galeraIP
-
-echo $template | grep "galera"
-if [ $? == 0 ] ; then
-	cluster_ip=$galeraIP
-else
-	cluster_ip=$replicationIP
-fi
 
 /usr/local/skysql/maxscale/system-test/configure_maxscale.sh $template $cluster_ip
 
